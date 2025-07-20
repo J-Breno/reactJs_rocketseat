03eca1ed2120ab88f9a887ebb7df69e8e4f3472d
J-Breno <joao.breno85@hotmail.com>
2025-07-19 23:13:59 -0300

    Componente - Post

---
+++ projetos/01-fundamentos-reactJs/src/components/Post.jsx
@@ -0,0 +1,34 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/j-breno.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>João Breno</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time dateTime="2025-07-19 22:51:12">Publicado há 1h</time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
