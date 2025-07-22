77acf025724946ee7ea94cf1743cddecb6944b48
J-Breno <joao.breno85@hotmail.com>
2025-07-22 16:25:27 -0300

    Iniciando novo ciclo

--- projetos/02-ignite-timer/src/pages/Home/index.tsx
+++ projetos/02-ignite-timer/src/pages/Home/index.tsx
@@ -9,30 +9,55 @@ import {
   TaskInput,
 } from "./styles";
 import { useForm } from "react-hook-form";
-import {zodResolver} from '@hookform/resolvers/zod'
-import * as zod from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5, 'O intervalo precisa ser de no mínimo 5 minutos').max(60, 'O intervalo precisa ser de no máximo 60 minutos'),
-})
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O intervalo precisa ser de no mínimo 5 minutos")
+    .max(60, "O intervalo precisa ser de no máximo 60 minutos"),
+});
 
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
 
 export function Home() {
-  const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
-      task: '', 
+      task: "",
       minutesAmount: 0,
-    }
+    },
   });
 
-  function handleCreateNewCycle(data: NewCycleFormData ) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+
     reset();
   }
 
-  const task = watch('task');
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const task = watch("task");
   const isSubmitDisabled = !task;
 
   return (
@@ -45,7 +70,7 @@ export function Home() {
               id="task"
               list="task-suggestions"
               placeholder="dê um nome para o seu projeto"
-              {...register('task')}
+              {...register("task")}
             />
             <datalist id="task-suggestions">
               <option value="Projeto 1" />
@@ -58,7 +83,7 @@ export function Home() {
               step={5}
               min={5}
               max={60}
-              {...register('minutesAmount', {valueAsNumber: true})}
+              {...register("minutesAmount", { valueAsNumber: true })}
             />
             <span>minutos.</span>
           </FormContainer>
