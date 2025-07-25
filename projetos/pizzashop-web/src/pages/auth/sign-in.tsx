8b22ab506aa9c8dbe400915edf26c0a74ada05b6
J-Breno <joao.breno85@hotmail.com>
2025-07-25 15:41:12 -0300

    Notificação toast (sonner)

--- projetos/pizzashop-web/src/pages/auth/sign-in.tsx
+++ projetos/pizzashop-web/src/pages/auth/sign-in.tsx
@@ -4,7 +4,8 @@ import { Label } from '@radix-ui/react-label';
 import { Helmet } from 'react-helmet-async'
 import {useForm} from 'react-hook-form'
 import {z} from 'zod'
-
+import {toast} from 'sonner'
+ 
 const signInForm = z.object({
     email: z.string().email()
 })
@@ -15,8 +16,17 @@ export function SignIn() {
     const {register, handleSubmit, formState: {isSubmitting}} = useForm<SignInForm>();
 
     async function handleSignIn(data: SignInForm) {
-        console.log(data)
-        await new Promise((resolve) => setTimeout(resolve, 2000))
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+            toast.success('Enviamos um link de autenticação para seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () => handleSignIn(data),
+                }
+            })
+        } catch {
+            toast.error('Credenciais inválidas.')
+        }
     }
     return(
         <>
