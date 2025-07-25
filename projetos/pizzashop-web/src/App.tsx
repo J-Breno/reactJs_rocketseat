4a57ef9073eba16a44ddb7516bc1ec1cb54c9021
J-Breno <joao.breno85@hotmail.com>
2025-07-25 13:03:49 -0300

    Intalando shadcn, ui

--- projetos/pizzashop-web/src/App.tsx
+++ projetos/pizzashop-web/src/App.tsx
@@ -1,5 +1,9 @@
+import { Button } from "./components/ui/button";
+import "./index.css";
 export function App() {
-  return (<>
-  
-  </>);
+  return <>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  </>;
 }
