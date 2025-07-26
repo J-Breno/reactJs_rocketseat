740617b907e120013449fc623da7de58f75027f1
J-Breno <joao.breno85@hotmail.com>
2025-07-26 18:30:15 -0300

    Modal de atualização do perfil

--- projetos/pizzashop-web/src/components/account-menu.tsx
+++ projetos/pizzashop-web/src/components/account-menu.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
+import { Dialog, DialogTrigger } from "./ui/dialog";
+import { StoreProfileDialog } from "./store-profile-dialog";
 
 export function AccountMenu() {
     const {data: profile, isLoading: isLoadingProfile} = useQuery({
@@ -19,6 +21,8 @@ export function AccountMenu() {
 
     return(
         <>
+        <Dialog>
+
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant={"outline"} className="flex items-center gap-2 select-none">
@@ -43,16 +47,20 @@ export function AccountMenu() {
                        }
                     </DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                        <Building className="w-4 h-4 mr-2" />
-                        <span>Perfil da loja</span>
-                    </DropdownMenuItem>
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="w-4 h-4 mr-2" />
+                            <span>Perfil da loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
                     <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
                         <LogOut className="w-4 h-4 mr-2" />
                         <span>Sair</span>
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
+            <StoreProfileDialog />
+        </Dialog>
         </>
     );
 }
